d4154bbda7792a816c0df37b244d56be0a5db681
Antoine Koenig <antoine24240@gmail.com>
2021-02-09 19:35:37 +0100

    navbar done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
+
   def home
+  end
+
+  def about
+  end
+
+  def contact
+    @members = [
+
+      {first_name:'Antoine',
+        last_name: 'Koenig'},
+       {first_name: 'Océane',
+        last_name: 'Rey'}
+     ]
   end
 end
